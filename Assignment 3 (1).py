9f79640b2b732d35ec4cdbd62f1fd8b99abcc33e
vivek sharma <133657968+0408vivek@users.noreply.github.com>
2023-06-27 19:03:00 +0530

    Add files via upload

---
+++ Assignment 3 (1).py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Q-1 Why are functions advantageous to have in your programs ?
+
+# In[1]:
+
+
+# Ans - Avoid repetion of codes , increase program redavility , divide a complex problem into simple on , reduce chanses of error .
+
+
+#   # Q-2 When does the code in a function run . when its specified or when it's called ?
+
+# In[2]:
+
+
+# Ans - The code in a function executed when the function is called , not when the function is specifies.
+
+
+# # Q-3 What statement creates a function?
+
+# In[5]:
+
+
+# Ans - The (def) statement creates a function 
+# def text1():
+
+
+# # Q-4 What is the differece between a function and a function call ?
+
+# In[6]:
+
+
+# Ans - In python some functions like print , while etc already enabled 
+#       but when we creat a local function we created with the help of def shortcut but output not some until we called .
+
+
+# # Q-5 How many global scopes are there in apython program ? How many local scopes ?
+
+# In[10]:
+
+
+# Ans - There is one global there is one global scope , and a local scope is created whenever a function is called.
+
+
+# # Q-6 What happens to variables in alocal scope when the function call returns?
+
+# In[8]:
+
+
+# Ans - When we use return function than output comes with same data types while print function always output in nonetype
+
+
+# # Q-7 What is the concept of return value ? is it possible to have a return value in an expression ?
+
+# In[9]:
+
+
+# Ans - A return value is the value that a function call evaluated to like any a return valye can be used as part of an expression .
+
+
+# # Q-8 if a function does not have a return statement , what is the return value of a call to that function ?
+
+# In[11]:
+
+
+# Ans - If there is no return statement for a function its return valye is none 
+
+
+# # Q-9 How do you make a function variable refer to the flobal variable ?
+
+# In[12]:
+
+
+# Ans - A global statement will force a variable in a function to refer to the global variable . 
+#       if you want to refer to a global variable in a function . 
+#       you can use the global key word to declare which variables are global .
+
+
+# # Q-10 What is the data type of None ?
+
+# In[ ]:
+
+
+# Ans - The data type of none is nonetype .
+
+
+# # Q-11 What does the sentence import are really uour pets name deric do ?
+
+# In[13]:
+
+
+# Ans - That import statement omports a module named are really uou pets name deric .
+
+
+# # Q-12 If you had bacon() feature in a spam module , what would you call it after importing spam ?
+
+# In[14]:
+
+
+# Ans - What can you do to save a programme from crashing if it encounters an error ?
+
+
+# # Q-13 What can you do to save a programme from crashing if it encounters an error ? 
+
+# In[15]:
+
+
+# Ans - Place the line of code that might cause an error in atry clause and use except block to handle the error .
+
+
+# # Q-14 What is the purpose of the rry clause? What is the purpose of the except clause?
+
+# In[16]:
+
+
+# Ans - The code that could potetially cause an eeror goes in the try clause . 
+#       The code that executed if an error happens goes in the except clause.
+
+
+# In[ ]:
+
+
+
+
