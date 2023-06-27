deecf786a80d56e971f1d8ceeec4e289bc03e204
vivek sharma <133657968+0408vivek@users.noreply.github.com>
2023-06-27 18:03:30 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 1
+
+# # Q1-In the below elements which of them are values or an expression ? 
+#     eg-values can be integer or string and expressions will be mathematical operators.
+
+# In[1]:
+
+
+# * is a mathematical operators
+# 'hello' is a string
+# -87.8 is a float value
+# - is a mathematical operators
+# / is a mathematical operators 
+# + is a mathematcial operators
+# 6 is a intiger value
+
+
+# # Q2- What is the difference between string and variable ?
+
+# In[2]:
+
+
+# a="hello"
+# here a is a variable and "hello" is a string 
+
+
+# # Q3- Describe three different data types ?
+
+# In[3]:
+
+
+# there are 6 data types in python 
+# string , tuple , dictonary.
+
+
+# # Q4-What is an expression made up of ? what do all expressions do ?
+
+# In[4]:
+
+
+# Expression made up of values , variables , operators
+# a=10 ( here a is a variable and = is a operators and 10 is value)
+# expression give result 
+
+
+# # Q5-After running the following code , what does the variable bacon contain ?
+# 
+# bacon=22
+# bacon+1
+# 
+# 
+
+# # Q6- What should the values of the following two terms be ?    
+#      
+
+# In[17]:
+
+
+"spam"+"spamspam"
+
+
+# # Q7-Why is eggs a valid variable name while 100 is invalid ?
+
+# In[19]:
+
+
+#variable name always start with alphabets and _(underscore)
+# variablename can not be start with numeric number so eggs a valid variable name while 100 is invalid
+
+
+# # Q8-What three functions can be used to get the ineger , foating point number , string version of value ?
+
+# In[20]:
+
+
+# int() is use for integer 
+# float is use for floating point
+# str() is use for string 
+
+
+# # Q9-Why does this expression cause an error ? How can you fix it ? " i have eaten"+99+"burraits"
+
+# In[22]:
+
+
+#in python we can concinate same data types so this expression give an error
+"i have eaten"+"99"+"burraits"
+# when we convert value 99 into string than it fix
+
+
+# In[ ]:
+
+
+
+
